e3b1ffd4c8e5fe048090fbe8613a076239c7abfd
luke <luke@wemakeprice.com>
2020-08-05 18:34:18 +0900

    friends 리팩토링

--- src/common/createItemsLogic.js
+++ src/common/createItemsLogic.js
@@ -6,4 +6,27 @@ export default function createItemsLogic(name) {
   const EDIT = `${name}/EDIT`;
 
   const add = item => ({ type: ADD, item });
+  const remove = item => ({ type: REMOVE, item });
+  const edit = item => ({ type: EDIT, item });
+
+  const reducer = createReducer(
+    { [name]: [] },
+    {
+      [ADD]: (state, action) => state[name].push(action.item),
+      [REMOVE]: (state, action) =>
+        (state[name] = state[name].filter(
+          item => item.id !== action.item.id
+        )),
+      [EDIT]: (state, action) => {
+        const index = state[name].findIndex(
+          item => item.id === action.item.id
+        );
+        if (index >= 0) {
+          state[name][index] = action.item;
+        }
+      },
+    }
+  );
+
+  return { add, remove, edit, reducer };
 }
--- src/index.js
+++ src/index.js
@@ -21,6 +21,14 @@ store.subscribe(() => {
   console.log(state)
 })
 
-store.dispatch(addTimeLine({ id:1, desc: '코딩지옥' }))
-store.dispatch(addTimeLine({ id:2, desc: '리액트지옥' }))
-store.dispatch(increaseNextPage());
+// store.dispatch(addTimeLine({ id: 1, desc: '코딩은 지옥' }));
+// store.dispatch(addTimeLine({ id: 2, desc: '리덕스 지옥' }));
+// store.dispatch(increaseNextPage());
+// store.dispatch(editTimeLine({ id: 2, desc: '리덕스 너무 지옥' }));
+// store.dispatch(removeTimeLine({ id: 1, desc: '코딩은 지옥' }));
+
+store.dispatch(addFriend({ id: 1, name: '아이유' }));
+store.dispatch(addFriend({ id: 2, name: '손나은' }));
+store.dispatch(editFriend({ id: 2, name: '권나라' }));
+store.dispatch(removeFriend({ id: 1, name: '아이유' }));
+store.dispatch(removeFriend({ id: 2, name: '권나라' }));
