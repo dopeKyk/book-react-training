a6dd19e06af428113a30f341c85647809e1dcbff
luke <luke@wemakeprice.com>
2020-08-06 12:26:14 +0900

    1차 실습..

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import { createStore, combineReducers } from 'redux'
 import timelineReducer, {
-  addTimeLine,
-  removeTimeLine,
-  editTimeLine,
+  addTimeline,
+  removeTimeline,
+  editTimeline,
   increaseNextPage,
 } from './timeline/state';
 import friendReducer, {
@@ -15,11 +15,13 @@ const reducer = combineReducers({
   timeline: timelineReducer,
   friend: friendReducer,
 });
+
 const store = createStore(reducer);
-store.subscribe(() => {
-  const state = store.getState();
-  console.log(state)
-})
+
+// store.subscribe(() => {
+//   const state = store.getState();
+//   console.log(state)
+// })
 
 // store.dispatch(addTimeLine({ id: 1, desc: '코딩은 지옥' }));
 // store.dispatch(addTimeLine({ id: 2, desc: '리덕스 지옥' }));
@@ -27,8 +29,8 @@ store.subscribe(() => {
 // store.dispatch(editTimeLine({ id: 2, desc: '리덕스 너무 지옥' }));
 // store.dispatch(removeTimeLine({ id: 1, desc: '코딩은 지옥' }));
 
-store.dispatch(addFriend({ id: 1, name: '아이유' }));
-store.dispatch(addFriend({ id: 2, name: '손나은' }));
-store.dispatch(editFriend({ id: 2, name: '권나라' }));
-store.dispatch(removeFriend({ id: 1, name: '아이유' }));
-store.dispatch(removeFriend({ id: 2, name: '권나라' }));
+// store.dispatch(addFriend({ id: 1, name: '아이유' }));
+// store.dispatch(addFriend({ id: 2, name: '손나은' }));
+// store.dispatch(editFriend({ id: 2, name: '권나라' }));
+// store.dispatch(removeFriend({ id: 1, name: '아이유' }));
+// store.dispatch(removeFriend({ id: 2, name: '권나라' }));
